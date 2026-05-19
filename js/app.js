4d5081029ff7954e7e42122d27899ab2f0fdb84f
Allef17 <guilhermeallef37@gmail.com>
2026-05-19 00:17:50 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,6 +6,26 @@ async function loadMovies(){
 
   console.log(movies);
 
+  const container = document.querySelector('.content-grid');
+
+  container.innerHTML = '';
+
+  movies.forEach(movie => {
+
+    container.innerHTML += `
+
+      <div class="card">
+
+        <img src="https://image.tmdb.org/t/p/w500/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg">
+
+        <h3>${movie.title}</h3>
+
+      </div>
+
+    `;
+
+  });
+
 }
 
 loadMovies();
